420e9919b87166fc4bab1b45f963fe463bdd8132
Eduardo Vieira <eduardofelipevieira@gmail.com>
2021-02-26 20:26:39 -0300

    wip

--- config/constants.js
+++ config/constants.js
@@ -10,7 +10,7 @@ module.exports = {
     twitter: {
         consumerKey: 'dn0m57D0L0H3s2eAd8qiXp0ED',
         consumerSecret: 'PLQOZzplxpidP0UOmc6JRiXlNEkkLCKaTN6RLfZks6X7m8DPaI',
-        accessToken: '72183014-EAwqnjnQyjM5EG5o1QQckk8XK31QVHHLK0268LPwe',
-        accessTokenSecret: 'LTX55N4fSAoxwdQpp7jCDfX7YlmtYB7ZRlVKzwznGykWB'
+        accessToken: '72183014-DvriqIJgwuDD2IRWyZH3CZSaKwSX72lWffAkFv5GM',
+        accessTokenSecret: 'gKDvdgH2f7FOu0ciPGxiPw7D8dAEcBDiF2QJbSizxxmRY'
     }
 }
