811fb5322178c76045883047082d6d87143e171e
Eduardo Vieira <eduardofelipevieira@gmail.com>
2021-03-02 19:15:41 -0300

    qip

--- config/constants.js
+++ config/constants.js
@@ -8,9 +8,9 @@ module.exports = {
         connectionString: ''
     },
     twitter: {
-        consumerKey: 'dn0m57D0L0H3s2eAd8qiXp0ED',
-        consumerSecret: 'PLQOZzplxpidP0UOmc6JRiXlNEkkLCKaTN6RLfZks6X7m8DPaI',
-        accessToken: '72183014-DvriqIJgwuDD2IRWyZH3CZSaKwSX72lWffAkFv5GM',
-        accessTokenSecret: 'gKDvdgH2f7FOu0ciPGxiPw7D8dAEcBDiF2QJbSizxxmRY'
+        consumerKey: process.env.CONSUMER_KEY,
+        consumerSecret: process.env.CONSUMER_SECRET,
+        accessToken: process.env.ACCESS_TOKEN,
+        accessTokenSecret: process.env.ACCESS_TOKEN_SECRET
     }
 }
