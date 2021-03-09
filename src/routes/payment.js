e72762d94b12d8dfa4fcd466c8ba90b5f2dd6517
Eduardo Vieira <eduardofelipevieira@gmail.com>
2021-03-09 01:19:05 -0300

    wp

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -2,15 +2,18 @@
 
 const express = require('express')
 const router = express.Router()
+const debug = require('debug')('yat:api')
 
 const stripe = require('stripe')(require('../../config/constants').stripe.secret);
 
 
 router.post('/intent', async (req, res) => {
-  const { items } = req.body
+  debug(req.body)
+
+  const { billing } = req.body
   
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: 100,
+    amount: billing === 'year' ? 2400 : 300,
     currency: 'usd'
   })
 
