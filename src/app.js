6376a2a0658da637cddcb7db0ea3fe5adfc3a5a0
Eduardo Vieira <eduardofelipevieira@gmail.com>
2021-03-15 19:06:09 -0300

    wip

--- src/app.js
+++ src/app.js
@@ -22,13 +22,13 @@ app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
 
 app.use(function (req, res, next) {
 
-    res.set('Access-Control-Expose-Headers', 'Access-Control-Allow-Origin,x-access-token,x-access-token-secret,x-auth-uid')
+    res.set('Access-Control-Expose-Headers', 'Access-Control-Allow-Origin,x-access-token,x-access-token-secret,x-auth-uid,authorization')
 
     res.setHeader('Access-Control-Allow-Origin', '*')
 
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
 
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,x-access-token,x-access-token-secret,x-auth-uid')
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,x-access-token,x-access-token-secret,x-auth-uid,authorization')
 
     res.setHeader('Access-Control-Allow-Credentials', true)
 
