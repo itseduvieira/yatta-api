c0eb37a2198bc9bacb617981638f124c86e03629
Eduardo Vieira <eduardofelipevieira@gmail.com>
2021-03-15 18:56:11 -0300

    wip

--- src/app.js
+++ src/app.js
@@ -18,7 +18,20 @@ const constants = require('../config/constants')
 app.use(bodyParser.json({limit: '50mb'}))
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
 
-app.use(cors({origin: '*'}))
+// app.use(cors({ origin: '*' }))
+
+app.use(function (req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200')
+
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+
+    res.setHeader('Access-Control-Allow-Credentials', true)
+
+    next();
+})
 
 const auth = async (req, res, next) => {
   if(req.header('x-access-token') && 
